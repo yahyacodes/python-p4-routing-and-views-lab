9a4dc04b052bd194ba0276ba1e00bbede41ac332
yahyacodes <yahya.codin@gmail.com>
2024-01-09 10:28:56 +0300

    Assignment complpeted

--- server/app.py
+++ server/app.py
@@ -4,5 +4,45 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+
+@app.route('/print/<parameter>')
+def print_string(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    result = '\n'.join(str(i) for i in range(parameter))
+    return result + '\n'
+
+@app.route('/math/<num1>/<operation>/<num2>')
+def math(num1, operation, num2):
+    result = 0
+    num1 = int(num1)
+    num2 = int(num2)
+
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == '%':
+        result = num1 % num2
+    elif operation == 'div':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return 'Error: Zero Division Error'
+    else:
+        return 'Enter a valid input'
+    
+    return f'{result}'
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
